3d5a9b24d382669e5c512f0fa97522133a7ae259
Ilya Gorbachevski <71652007+ILYA780@users.noreply.github.com>
2026-01-03 00:38:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Flask, jsonify, render_template
+import os
 
 app = Flask(__name__)
 
-# Главная страница
 @app.route("/")
 def index():
     return render_template("index.html")
 
-# API с квартирами (пока демо-данные)
 @app.route("/api/flats")
 def flats():
     return jsonify([
@@ -16,14 +15,9 @@ def flats():
             "lng": 27.5619,
             "price": "500 $",
             "contact": "+375 29 123-45-67"
-        },
-        {
-            "lat": 53.9100,
-            "lng": 27.5400,
-            "price": "650 $",
-            "contact": "+375 33 987-65-43"
         }
     ])
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if name == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
